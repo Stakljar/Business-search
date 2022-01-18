384a2762644b71e4ebcabd50fa372d6caae0396b
Stakljar <73203225+Stakljar@users.noreply.github.com>
2022-01-18 22:36:56 +0100

    Update YelpBusinessService.kt

--- app/src/main/java/hr/ferit/job_seeker/YelpBusinessService.kt
+++ app/src/main/java/hr/ferit/job_seeker/YelpBusinessService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Query
 
 interface YelpBusinessService {
     @GET("/v3/businesses/search")
-    fun getPersons(
+    fun getBusinesses(
         @Header("Authorization") authorization: String,
         @Query("location") location: String,
         @Query("term") term: String,
