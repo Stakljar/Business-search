f7b9306f42ecc9cc14817ea5a3dce26935b16a68
Stakljar <73203225+Stakljar@users.noreply.github.com>
2022-02-21 00:37:37 +0100

    Update DataMediator.kt

--- app/src/main/java/hr/ferit/job_seeker/mediator/DataMediator.kt
+++ app/src/main/java/hr/ferit/job_seeker/mediator/DataMediator.kt
@@ -5,22 +5,15 @@ import hr.ferit.job_seeker.storage.entities.Business
 
 object DataMediator {
 
-    private val businesses = ArrayList<Business>()
     private lateinit var businessDao: BusinessDao
 
     fun setDao(businessDao: BusinessDao){
         DataMediator.businessDao = businessDao
     }
+    fun getDao(): BusinessDao{
+        return businessDao
+    }
     suspend fun removeFromDB(business: Business){
         businessDao.delete(business)
-    }
-    fun addBusiness(business: Business){
-        businesses.add(business)
-    }
-    fun getAllBusinesses(): ArrayList<Business>{
-        return businesses
-    }
-    fun removeAll(){
-        businesses.clear()
     }
 }
